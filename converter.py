36870f350e23cb8ec13c27bf8fa320231f52032f
codebrain001 <abozejohn@gmail.com>
2021-01-07 19:05:01 +0100

    completed the converter script

---
+++ converter.py
@@ -0,0 +1,12 @@
+import pydub
+
+class Converter:
+    def __init__(self, audio_path):
+        self.audio = pydub.AudioSegment.from_file(audio_path)
+
+    def convert(self, desired_format):
+        output = './audio.'+ desired_format
+        self.audio.export(output, format=desired_format)
+
+audio_converter = Converter('./audio.m4a')
+audio_converter.convert('flac')
