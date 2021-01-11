893fad60c126f1829738e3b11aba5d15f0db56ed
codebrain001 <abozejohn@gmail.com>
2021-01-11 17:52:56 +0100

    Refactored scripts

--- transcribe.py
+++ transcribe.py
@@ -6,38 +6,48 @@ from google.oauth2 import service_account
 
 
 class AudioTranscriber:
-    def __init__(self):
+    def __init__(self,URI):
         self.credentials = service_account.Credentials.from_service_account_file(
             'key.json')
         self.client = speech.SpeechClient(credentials=self.credentials)
         # Audio file to be transcribed from google cloud bucket
-        self.gcs_uri = "gs://zoom_project_data/audio.flac"
+        self.gcs_uri = URI
         self.audio = speech.RecognitionAudio(uri=self.gcs_uri)
-
-    def transcribe(self, language_code):
-        config = speech.RecognitionConfig(
+        self.config = speech.RecognitionConfig(
             encoding=speech.RecognitionConfig.AudioEncoding.FLAC,
-            language_code=language_code,
-            # audioChannelCount=channel_count,
-            # enableSeparateRecognitionPerChannel=True,
+            language_code="en-US",
+            audio_channel_count=2,
+            enable_separate_recognition_per_channel=True,
         )
 
+    def transcribe(self):
         transcript = ''
 
         # Detects speech in the audio file
         operation = self.client.long_running_recognize(
-            config=config, audio=self.audio)
+            config=self.config, audio=self.audio)
         start_time = time.time()
         print("Waiting for operation to complete...")
         response = operation.result()
 
         for result in response.results:
             transcript += result.alternatives[0].transcript
-
+        print('Transcribing completed')
         print(f'Transcription took {time.time()-start_time}seconds')
-        return transcript
+
+        #Writing transcript into text file
+        print('saving transcript')
+        with open('transcript-3.txt', 'w') as file:
+            file.write(transcript)
+        
+
+zoom_project = AudioTranscriber("gs://zoom_project_data/audio-3.flac")
+transcript = zoom_project.transcribe()
+
+#TODO:
+# Add the 3 different transcript
+
+#FIXME:
+#The new line cleaning
 
 
-zoom_project = AudioTranscriber()
-transcript = zoom_project.transcribe("en-US")
-print(transcript)
--- transcript.py
+++ transcript.py
@@ -1,4 +1,6 @@
-git# Imports the Google Cloud client library
+import csv
+
+# Imports the Google Cloud client library
 from google.cloud import speech
 # Import the google service account api authentication
 from google.oauth2 import service_account
@@ -9,6 +11,7 @@ client = speech.SpeechClient(credentials=credentials)
 
 # The name of the audio file to transcribe from cloud bucket
 gcs_uri = "gs://zoom_project_data/audio.flac"
+# gcs_uri = "gs://zoom_project_data/test.flac"
 
 audio = speech.RecognitionAudio(uri=gcs_uri)
 
@@ -24,10 +27,33 @@ operation = client.long_running_recognize(config=config, audio=audio)
 print("Waiting for operation to complete...")
 response = operation.result()
 
+# with open('text.csv', 'w') as file:
+#     writer = csv.writer(file)
+#     # create header
+#     writer.writerow(['text'])
+#     for result in response.results:
+#         writer.writerow([result.alternatives[0].transcript])
+    # The first alternative is the most likely one for this portion.
+    #print(u"Transcript: {}".format(result.alternatives[0].transcript))
+    
+
 for result in response.results:
     transcript += result.alternatives[0].transcript
+print('Transcribing completed')
 
-print(transcript)
+f = open("transcript.txt", "w")
+f.write(transcript)
+f.close()
+
+
+# f = open('text.csv','w')
+# w = csv.writer(f, delimiter='.')
+# # create header
+# w.writerow(['text'])
+# w.writerows([x.split('.') for x in transcript])
+# f.close()
+
+# print(transcript)
 
 # print(u"Transcript: {}".format(result.alternatives[0].transcript))
 # print("Confidence: {}".format(result.alternatives[0].confidence))
