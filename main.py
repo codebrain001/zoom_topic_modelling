e6f0f898619feed7c1994f15dc9bfe4085d19ae2
codebrain001 <abozejohn@gmail.com>
2021-02-17 03:35:29 +0100

    updated readme

--- main.py
+++ main.py
@@ -5,18 +5,17 @@ from modeling import LdaModeling
 
 def main()
 
-
-if __name__ == '__main__':
-    audio_converter = Converter('./audio.mp3')
-    audio_converter.convert('flac')
-    zoom_project = Transcriber("gs://zoom_project_data/audio.flac")
-    transcript = zoom_project.transcribe()
-    sentence_spliter = Spliter.split()
-    lda_instance = LdaModeling('transcript.csv')
-    gensim_corpus, gensim_dictionary = lda_instance.preprocessing()
-    lda_model = lda_instance.modeling()
-    lda_instance.plotting(lda_model, gensim_corpus, gensim_dictionary)
-    lda_instance.performance(lda_model, gensim_corpus, gensim_dictionary)
-    
+    if __name__ == '__main__':
+        audio_converter = Converter('./audio.mp3')
+        audio_converter.convert('flac')
+        zoom_project = Transcriber("gs://zoom_project_data/audio.flac")
+        transcript = zoom_project.transcribe()
+        sentence_spliter = Spliter.split()
+        lda_instance = LdaModeling('transcript.csv')
+        gensim_corpus, gensim_dictionary = lda_instance.preprocessing()
+        lda_model = lda_instance.modeling()
+        # lda_instance.performance(lda_model, gensim_corpus, gensim_dictionary)
+        lda_plot = lda_instance.plotting(lda_model, gensim_corpus, gensim_dictionary)
+        print(lda_plot)
 
 main()
--- modeling.py
+++ modeling.py
@@ -1,5 +1,5 @@
 import nltk
-import pandas
+import pandas as pd
 import re
 import gensim
 import gensim.corpora as corpora
@@ -7,17 +7,27 @@ from gensim.utils import simple_preprocess
 from gensim.models import CoherenceModel
 import pyLDAvis
 import pyLDAvis.gensim 
+
 nltk.download('stopwords')
 en_stop = list(nltk.corpus.stopwords.words('english'))
 en_stop.extend(['something', 'please', 'really', 'actually', 'actually', 'probably', 'little', 'minute'])
 en_stop = set(en_stop)
 from nltk.stem import WordNetLemmatizer
 stemmer = WordNetLemmatizer()
+import warnings
+warnings.filterwarnings("ignore")
 
 class LdaModeling():
     def __init__(self, data):
         self.df = pd.read_csv(data)
         self.df = self.df.drop(columns=['ID'])
+        self.corpus_superlist = self.df[['text']].values.tolist()
+        #corpus_superlist
+        self.corpus = []
+        for sublist in self.corpus_superlist:
+            for item in sublist:
+                self.corpus.append(item)
+
 
     def preprocessing(self):
         def preprocess_text(document):
@@ -48,7 +58,7 @@ class LdaModeling():
             return tokens
 
         processed_data = [];
-        for doc in corpus:
+        for doc in self.corpus:
             tokens = preprocess_text(doc)
             processed_data.append(tokens)
 
@@ -59,16 +69,25 @@ class LdaModeling():
 
 
     def modeling(self):
-        lda_model = gensim.models.ldamodel.LdaModel(gensim_corpus, num_topics=3, id2word=gensim_dictionary, passes=20)
+        lda_model = gensim.models.ldamodel.LdaModel(gensim_corpus, num_topics=3, id2word=gensim_dictionary, passes=50)
         lda_model.save('gensim_model.gensim')
         return lda_model
 
     def plotting(self, lda_model, gensim_corpus, gensim_dictionary):
-        pyLDAvis.enable_notebook()
-        pyLDAvis.gensim.prepare(lda_model, gensim_corpus, gensim_dictionary)
+        print('display')
+        vis_data = pyLDAvis.gensim.prepare(lda_model, gensim_corpus, gensim_dictionary)
+        pyLDAvis.show(vis_data)
 
     def performance(self, lda_model, gensim_corpus, gensim_dictionary):
         print('\nPerplexity:', lda_model.log_perplexity(gensim_corpus))
         coherence_score_lda = CoherenceModel(model=lda_model, texts=gensim_corpus, dictionary=gensim_dictionary, coherence='c_v')
         coherence_score = coherence_score_lda.get_coherence()
         print('\nCoherence Score:', coherence_score)
+
+lda_instance = LdaModeling('transcript.csv')
+gensim_corpus, gensim_dictionary = lda_instance.preprocessing()
+lda_model = lda_instance.modeling()
+# lda_instance.performance(lda_model, gensim_corpus, gensim_dictionary)
+lda_plot = lda_instance.plotting(lda_model, gensim_corpus, gensim_dictionary)
+print(lda_plot)
+# lda_instance.performance(lda_model, gensim_corpus, gensim_dictionary)
